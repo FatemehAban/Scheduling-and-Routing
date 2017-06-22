552e58da86bca711c0b405b5d92e8ac547e7e9e6
fatemeh <fatemeh.saeedifar@gmail.com>
2017-06-23 04:36:26 +0800

    fix the problems for login and register request

--- Login.php
+++ Login.php
@@ -5,22 +5,26 @@
     
     $username = $_POST["username"];
     $password = $_POST["password"];
+
+    $response = array();
     
     $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ?");
     mysqli_stmt_bind_param($statement, "s", $username);
     mysqli_stmt_execute($statement);
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $colUserID, $colName, $colAge, $colGender, $colUsername,  $colPassword,);
-    
-    $response = array();
-    $response["success"] = false;  
-    
+    mysqli_stmt_bind_result($statement, $colUserID, $colName, $colAge, $colGender, $colUsername,  $colPassword);  
+
+
+    $response["success"] = false;
+
+
     while(mysqli_stmt_fetch($statement)){
-        if (password_verify($password, $colPassword)) {
+       // if (password_verify($password, $colPassword)) {
             $response["success"] = true;  
             $response["name"] = $colName;
-            $response["age"] = $colAge;
-        }
+            $response["username"]= $colUsername;
+          
+       // }
     }
     echo json_encode($response);
 ?>
--- LoginRegister/app/src/main/java/com/example/lenovo/loginregister/UserAreaActivity.java
+++ LoginRegister/app/src/main/java/com/example/lenovo/loginregister/UserAreaActivity.java
@@ -12,15 +12,15 @@ public class UserAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final TextView etUsername = (TextView) findViewById(R.id.etUsername);
         final TextView WelcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         String username = intent.getStringExtra("username");
 
-        String message = name + "welcome to your user area";
+        String message = name + " " + "welcome to your user area";
         WelcomeMessage.setText(message);
-        etUsername.setText(username);
+        //etUsername.setText(username);
     }
 }
